270c82232de2e99f7b014ccc7adf0f3bedef177c
GeKamB <k.gebski1@gmail.com>
2023-10-10 09:05:27 +0100

    add normalize.css, create html and basic css

--- src/index.js
+++ src/index.js
@@ -1,67 +1,125 @@
+
+import './normalize.css'
 import './style.css';
+import { compareAsc, format } from 'date-fns'
+
+
 
 
 const todoes = [];
 const projects = [];
-window.todoes =todoes;
+window.todoes = todoes;
 
 
 class ToDo {
-    progres = 'in progress'
-    constructor(title, desciption, duedate, priority, notes) {
+    constructor(title, description, dueDate, priority, notes) {
         this.title = title;
-        this.desciption = desciption;
-        this.duedate = duedate
+        this.description = description;
+        this.dueDate = dueDate;
         this.priority = priority;
         this.notes = notes;
-    }
-    editProgress() {
-        if (this.progres === 'in progress') {
-            this.progres = 'completed'
-        }
-        else  {
-            this.progres = 'in progress';            
-        }
-        
-    }
-    editTitle(value) {
-        this.title = value
-    }
-    editDescription() {
-        this.desciption = value;
-    }
-    editDate(value) {
-        this.date = value
-    }
-    editPriority(value) {
-        this.priority = value
-    }
-    editNotes(value) {
-        this.notes = value
+        this.progress = 'in progress';
     }
 
-    
+    get changeProgress() {
+        return this.progress;
+    }
+
+    set changeProgress(value) {
+        if (value === 'in progress' || value === 'completed') {
+            this.progress = value;
+        } else {
+            console.error('Invalid progress value');
+        }
+    }
+
+    get changeTitle() {
+        return this.title;
+    }
+
+    set changeTitle(value) {
+        this.title = value;
+    }
+    get changeDescript() {
+        return this.description;
+    }
+
+    set changeDescript(value) {
+        this.description = value;
+    }
+
+    get changeDate() {
+        return this.dueDate;
+    }
+
+    set changeDate(value) {
+        this.dueDate = value;
+    }
+
+    get changePriority() {
+        return this.priority;
+    }
+
+    set changePriority(value) {
+        this.priority = value;
+    }
+
+    get changeNotes() {
+        return this.notes;
+    }
+
+    set changeNotes(value) {
+        this.notes = value;
+    }
+
+ 
+}
+class Project extends ToDo {
+    constructor(title, description, dueDate, priority, notes, projectName) {
+        super(title, description, dueDate, priority, notes);
+        this.projectName = projectName;
+    }
+
+    get changeProjectName() {
+        return this.projectName;
+    }
+
+    set changeProjectName(value) {
+        this.projectName = value;
+    }
 }
 
 
-function createToDo(title, desciption, duedate, priority, notes) {
-    let newToDo = new ToDo(title, desciption, duedate, priority, notes);
-    todoes.push(newToDo);
+function createToDo(title, descritption, dueDate, priority, notes, append) {
+    let newToDo = new ToDo(title, descritption, dueDate, priority, notes);
+    append.push(newToDo);
     
 };
-function createProject() {
-    let project = []
-    projects.push(project)
-}
-createToDo('to do app', 'zrobic to do app',new Date(2023, 10, 17) , '1', '..');
-createToDo('to do app', 'zrobic to do app',new Date(2023, 10, 10) , '1', '..');
-createProject()
+
+createToDo('ToDo app', 'zrobic to do app',new Date(2023, 10, 17) , '1', '..', todoes);
+createToDo('trening', 'zrobic to do app',new Date(2003, 10, 17) , '1', '..', todoes);
+createToDo('matematyka', 'zrobic to do app',new Date(2023, 10, 10) , '1', '..', todoes);
+let newProject = new Project('punkt 1', 'zrobic to do app', new Date(2023, 10, 17) , '1', 'gowniany Projekt')
+console.log(newProject);
+console.log(newProject.projectName)
 
 
 
 let a = new ToDo('ToDo', 'create a ToDo app', new Date(2023, 10, 10), 'high', '');
-projects[0].push(a)
-console.log(projects);
+a.changeTitle = 'dupa';
+console.log(a)
+
+
+todoes.sort(function(a,b){
+    // Turn your strings into dates, and then subtract them
+    // to get a value that is either negative, positive, or zero.
+    return new Date(b.dueDate) - new Date(a.dueDate);
+  });
+  console.log(todoes);
+
+
+
+
 
 
 
