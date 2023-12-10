9e98462a96f425d34bc94cdc71b0b58e128f7ddb
GeKamB <k.gebski1@gmail.com>
2023-12-10 16:46:31 +0000

    eventListeners and Lists

--- src/index.js
+++ src/index.js
@@ -4,12 +4,77 @@ import './style.css';
 import { compareAsc, format } from 'date-fns'
 
 
+const listsContainer = document.querySelector('[data-lists]');
+const newListForm = document.querySelector('[data-new-list-form');
+const newListInput = document.querySelector('[data-new-list-input]');
 
 
+
+const LOCAL_STORAGE_PROJECT_KEY = 'task.projects';
+const LOCAL_STORAGE_SELECTED_PROJECT_ID_KEY = 'task.selectedListId'
+const projects = JSON.parse(localStorage.getItem(LOCAL_STORAGE_PROJECT_KEY)) || [];
+let selectedProjectId = localStorage.getItem(LOCAL_STORAGE_SELECTED_PROJECT_ID_KEY)
+
+listsContainer.addEventListener('click', e => {
+    if (e.target.tagName.toLowerCase() === 'li') {
+        selectedProjectId = e.target.dataset.projectId
+        saveAndRender()
+    }
+})
+
+newListForm.addEventListener('submit', e => {
+    e.preventDefault()
+    const projectName = newListInput.value
+    if (projectName == null || projectName === '') return
+    const project = createProject(projectName)
+    newListInput.value = null
+    projects.push(project)
+    saveAndRender()
+
+});
+
+function createProject(name) {
+    return { id: Date.now().toString(), name: name, tasks: []}
+}
+
+function saveAndRender() {
+    save()
+    render()
+}
+
+function save() {
+    localStorage.setItem(LOCAL_STORAGE_PROJECT_KEY, JSON.stringify(projects))
+    localStorage.setItem(LOCAL_STORAGE_SELECTED_PROJECT_ID_KEY, selectedProjectId)
+}
+
+
+function render() {
+    clearElement(listsContainer);
+    projects.forEach(project => {        
+        const listElement = document.createElement('li')
+        listElement.dataset.projectId = project.id
+        listElement.classList.add('project')
+        listElement.innerText = project.name
+    if (project.id === selectedProjectId) {
+        listElement.classList.add('active-project')
+    }
+        listsContainer.appendChild(listElement)
+    })
+};
+
+function clearElement(element) {
+    while (element.firstChild) {
+        element.removeChild(element.firstChild)
+    };
+};
+
+render();
+
+
+
+//              CLASSES
 const todoes = [];
-const projects = [];
 window.todoes = todoes;
-
 
 class ToDo {
     constructor(title, description, dueDate, priority, notes) {
@@ -74,22 +139,9 @@ class ToDo {
 
  
 }
-class Project extends ToDo {
-    constructor(title, description, dueDate, priority, notes, projectName) {
-        super(title, description, dueDate, priority, notes);
-        this.projectName = projectName;
-    }
-
-    get changeProjectName() {
-        return this.projectName;
-    }
-
-    set changeProjectName(value) {
-        this.projectName = value;
-    }
-}
 
 
+// add new ToDo to desired array (append para.)
 function createToDo(title, descritption, dueDate, priority, notes, append) {
     let newToDo = new ToDo(title, descritption, dueDate, priority, notes);
     append.push(newToDo);
@@ -99,9 +151,6 @@ function createToDo(title, descritption, dueDate, priority, notes, append) {
 createToDo('ToDo app', 'zrobic to do app',new Date(2023, 10, 17) , '1', '..', todoes);
 createToDo('trening', 'zrobic to do app',new Date(2003, 10, 17) , '1', '..', todoes);
 createToDo('matematyka', 'zrobic to do app',new Date(2023, 10, 10) , '1', '..', todoes);
-let newProject = new Project('punkt 1', 'zrobic to do app', new Date(2023, 10, 17) , '1', 'gowniany Projekt')
-console.log(newProject);
-console.log(newProject.projectName)
 
 
 
@@ -115,7 +164,7 @@ todoes.sort(function(a,b){
     // to get a value that is either negative, positive, or zero.
     return new Date(b.dueDate) - new Date(a.dueDate);
   });
-  console.log(todoes);
+
 
 
 
